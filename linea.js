edd4c50f3351851f0534ee6d592712b2f08e9d91
Gómez Palma Dania Vanessa <134179962+vanessaG0906@users.noreply.github.com>
2024-08-17 16:52:20 -0500

    Add files via upload

---
+++ linea.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+	let xhttp = new XMLHttpRequest();
+	xhttp.open("GET","linea.xml");
+	xhttp.send();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState==4 && xhttp.status==200) {
+			crearLinea(xhttp.responseXML);
+		}
+	}
+}
+function crearLinea(xmlDoc){
+	let mate = xmlDoc.documentElement.childNodes;
+	let datos = [];
+	//
+	for (let i = 0; i < mate.length; i++) {
+		if (mate[i].nodeType==1) {
+			m = mate[i].childNodes;
+			datos.push({
+				apellido:m[1].childNodes[0].nodeValue,
+				nombre:m[3].childNodes[0].nodeValue,
+				nacimiento:parseInt(m[5].childNodes[0].nodeValue),
+				fallecimiento:parseInt(m[7].childNodes[0].nodeValue),
+				imagen:m[9].childNodes[0].nodeValue,
+				biografia:m[11].childNodes[0].nodeValue,
+			});
+		}
+	}
+	delete mate, xmlDoc;
+	console.log(datos);
+}
